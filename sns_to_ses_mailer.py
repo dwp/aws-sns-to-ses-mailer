4e2afd78d6244d0101c2bfd70c09639d7353fb69
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2018-09-06 12:31:55 +0100

    add email recipients from message params, add template parameters from message param, add logging, make sending address message param, make sending domain env var

--- sns_to_ses_mailer.py
+++ sns_to_ses_mailer.py
@@ -3,8 +3,10 @@ from __future__ import print_function
 import StringIO
 import csv
 import json
+from collections import namedtuple
 import os
 import zlib
+import logging
 
 from time import strftime, gmtime
 from email.mime.multipart import MIMEMultipart
@@ -22,11 +24,16 @@ __version__ = '1.0'
 # Get Lambda environment variables
 region = os.environ['REGION']
 max_threads = os.environ['MAX_THREADS']
+from_domain = os.environ['SENDING_DOMAIN']
 
-# Initialize clients
+# Initialise logging
+logger = logging.getLogger()
+logger.setLevel(os.environ['LOG_LEVEL'])
+logger.info("Logging at {} level".format(os.environ['LOG_LEVEL']))
+
+# Initialise clients
 s3 = boto3.client('s3', region_name=region)
 ses = boto3.client('ses', region_name=region)
-send_errors = []
 mime_message_text = ''
 mime_message_html = ''
 
@@ -49,7 +56,6 @@ def mime_email(subject, from_address, to_address, text_message=None, html_messag
 
 
 def send_mail(from_address, to_address, message):
-    global send_errors
     try:
         response = ses.send_raw_email(
             Source=from_address,
@@ -61,81 +67,76 @@ def send_mail(from_address, to_address, message):
             }
         )
         if not isinstance(response, dict):  # log failed requests only
-            send_errors.append('%s, %s, %s' % (current_time(), to_address, response))
+            logger.error('%s, Error sending to: %s, %s' % (current_time(), to_address, response))
     except botocore.exceptions.ClientError as e:
-        send_errors.append('%s, %s, %s, %s' %
-                           (current_time(),
-                            to_address,
-                            ', '.join("%s=%r" % (k, v) for (k, v) in e.response['ResponseMetadata'].iteritems()),
-                            e.message))
+        logger.error('%s, Error sending to: %s, %s, %s' %
+                     (current_time(),
+                      to_address,
+                      ', '.join("%s=%r" % (k, v) for (k, v) in e.response['ResponseMetadata'].iteritems()),
+                      e.message))
+
+
+def get_parameters(event):
+    print(event)
+    message = json.loads(event['Records'][0]['Sns']['Message'], object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+
+    return message.ses_mailer
 
 
 def lambda_handler(event, context):
-    global send_errors
+
     global mime_message_text
     global mime_message_html
     try:
+        args = get_parameters(event)
+
+        if args.recipients:
+            recipients = args.recipients
+        else:
+            recipients = []
+
         # Read the uploaded csv file from the bucket into python dictionary list
-        print(event)
-        message = json.loads(event['Records'][0]['Sns']['Message'])['ses_mailer']
-        bucket = message['bucket']
-        mailing_list = message['mailing_list']
-        text_message_file = message['plain_text_template']
-        html_message_file = message['html_template']
-        response = s3.get_object(Bucket=bucket, Key=mailing_list)
-        body = zlib.decompress(response['Body'].read(), 16+zlib.MAX_WBITS)
-        reader = csv.DictReader(StringIO.StringIO(body),
-                                fieldnames=['from_address', 'to_address'])
+        if args.mailing_list:
+            response = s3.get_object(Bucket=args.bucket, Key=args.mailing_list)
+            body = zlib.decompress(response['Body'].read(), 16+zlib.MAX_WBITS)
+            reader = csv.DictReader(StringIO.StringIO(body),
+                                    fieldnames=['email_address', 'name'])
+            for row in reader:
+                recipients.append({row['email_address'].strip(), row['name'].strip()})
 
         # Read the message files
         try:
-            response = s3.get_object(Bucket=bucket, Key=text_message_file)
+            response = s3.get_object(Bucket=args.bucket, Key=args.plain_text_template)
             mime_message_text = response['Body'].read()
             t = Template(mime_message_text)
             mime_message_text = t.render(something="World")
         except:
             mime_message_text = None
-            print('Failed to read text message file. Did you upload %s?' % text_message_file)
+            logger.info('Failed to read text message file. Did you upload %s?' % args.plain_text_template)
         try:
-            response = s3.get_object(Bucket=bucket, Key=html_message_file)
+            response = s3.get_object(Bucket=args.bucket, Key=args.html_template)
             mime_message_html = response['Body'].read()
             t = Template(mime_message_html)
-            mime_message_html = t.render(something="World")
+            mime_message_html = t.render(args.template_variables)
         except:
             mime_message_html = None
-            print('Failed to read html message file. Did you upload %s?' % html_message_file)
+            logger.info('Failed to read html message file. Did you upload %s?' % args.html_template)
 
         if not mime_message_text and not mime_message_html:
             raise ValueError('Cannot continue without a text or html message file.')
 
         # Send in parallel using several threads
         e = concurrent.futures.ThreadPoolExecutor(max_workers=max_threads)
-        for row in reader:
-            from_address = row['from_address'].strip()
-            to_address = row['to_address'].strip()
+        for recipient in recipients:
+            from_address = "{}@{}".format(args.from_local_part, from_domain)
+            to_address = recipient['email_address']
             subject = event['Records'][0]['Sns']['Subject']
             message = mime_email(subject, from_address, to_address, mime_message_text, mime_message_html)
             e.submit(send_mail, from_address, to_address, message)
         e.shutdown()
     except Exception as e:
-        print(e.message + ' Aborting...')
+        logger.exception(e.message + ' Aborting...')
         raise e
-
-    print('Send email complete.')
-
-    # Upload errors if any to S3
-    if len(send_errors) > 0:
-        try:
-            result_data = '\n'.join(send_errors)
-            logfile_key = key.replace('.csv.gz', '') + '_error.log'
-            response = s3.put_object(Bucket=bucket, Key=logfile_key, Body=result_data)
-            if 'ResponseMetadata' in response.keys() and response['ResponseMetadata']['HTTPStatusCode'] == 200:
-                print('Send email errors saved in s3://%s/%s' % (bucket, logfile_key))
-        except Exception as e:
-            print(e)
-            raise e
-        # Reset publish error log
-        send_errors = []
 
 
 if __name__ == "__main__":
