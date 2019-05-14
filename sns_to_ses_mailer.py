b149cf2ab358acac95dba8e77187a467f38b789b
mirza.baig <mirza.baig@working-age.digital.dwp.gov.uk>
2019-05-14 12:15:42 +0100

    Added cc and bcc

--- sns_to_ses_mailer.py
+++ sns_to_ses_mailer.py
@@ -16,11 +16,12 @@ import boto3
 import concurrent.futures
 
 __author__ = 'DWP DataWorks'
-__date__ = '01/08/2018'
-__version__ = '1.0'
+__date__ = '14/05/2019'
+__version__ = '1.1'
 
 # Get Lambda environment variables
 region = os.environ['REGION']
+ses_mailer_region = os.environ['SES_MAILER_REGION']
 max_threads = int(os.environ['MAX_THREADS'])
 from_domain = os.environ['SENDING_DOMAIN']
 
@@ -36,7 +37,7 @@ logger.info("Logging at {} level".format(os.environ['LOG_LEVEL']))
 if 'AWS_PROFILE' in os.environ:
     boto3.setup_default_session(profile_name=os.environ['AWS_PROFILE'])
 s3 = boto3.client('s3', region_name=region)
-ses = boto3.client('ses', region_name=region)
+ses = boto3.client('ses', region_name=ses_mailer_region)
 mime_message_text = ''
 mime_message_html = ''
 
@@ -45,11 +46,15 @@ def current_time():
     return strftime("%Y-%m-%d %H:%M:%S UTC", gmtime())
 
 
-def mime_email(subject, from_address, to_address, text_message=None, html_message=None):
+def mime_email(subject, from_address, to_address, cc_address=None, bcc_address=None, text_message=None, html_message=None):
     msg = MIMEMultipart('alternative')
     msg['Subject'] = subject
     msg['From'] = from_address
     msg['To'] = to_address
+    if cc_address:
+        msg['CC'] = cc_address
+    if bcc_address:
+        msg['BCC'] = bcc_address
     if text_message:
         msg.attach(MIMEText(text_message, 'plain'))
     if html_message:
@@ -95,7 +100,7 @@ def lambda_handler(event, context):
             recipients = []
 
         # Read the uploaded csv file from the bucket into python dictionary list
-        if args['mailing_list']:
+        if 'mailing_list' in args and args['mailing_list']:
             response = s3.get_object(Bucket=args['bucket'], Key=args['mailing_list'])
             body = zlib.decompress(response['Body'].read(), 16+zlib.MAX_WBITS)
             # reader = csv.DictReader(StringIO.StringIO(body),
@@ -115,7 +120,8 @@ def lambda_handler(event, context):
         except Exception as e:
             logger.info(e)
             mime_message_text = None
-            logger.info('Failed to read text message file. Did you upload %s?' % args['plain_text_template'])
+            logger.info('Failed to read text message file. Did you upload %s?'
+                        % args['plain_text_template'] if 'plain_text_template' in args else 'plain_text_template')
         try:
             response = s3.get_object(Bucket=args['bucket'], Key=args['html_template'])
             mime_message_html = response['Body'].read().decode("utf-8")
@@ -125,7 +131,8 @@ def lambda_handler(event, context):
         except Exception as e:
             logger.info(e)
             mime_message_html = None
-            logger.info('Failed to read html message file. Did you upload %s?' % args['html_template'])
+            logger.info('Failed to read html message file. Did you upload %s?'
+                        % args['html_template'] if 'html_template' in args else 'html_template')
 
         if not mime_message_text and not mime_message_html:
             raise ValueError('Cannot continue without a text or html message file.')
@@ -135,10 +142,17 @@ def lambda_handler(event, context):
         for recipient in recipients:
             from_address = "{}@{}".format(args['from_local_part'], from_domain)
             to_address = recipient['email_address']
+            cc_address = recipient['email_address_cc'] if 'email_address_cc' in recipient else None
+            bcc_address = recipient['email_address_bcc'] if 'email_address_bcc' in recipient else None
             subject = event['Records'][0]['Sns']['Subject']
-            t = Template(mime_message_html, variable_start_string='[[', variable_end_string=']]')
-            mime_message_html = t.render(recipient_name=recipient['name'])
-            message = mime_email(subject, from_address, to_address, mime_message_text, mime_message_html)
+            if mime_message_html:
+                t = Template(mime_message_html, variable_start_string='[[', variable_end_string=']]')
+                mime_message_html = t.render(recipient_name=recipient['name'])
+            if mime_message_text:
+                t = Template(mime_message_text, variable_start_string='[[', variable_end_string=']]')
+                mime_message_text = t.render(recipient_name=recipient['name'])
+            message = mime_email(subject, from_address,
+                                 to_address, cc_address, bcc_address, mime_message_text, mime_message_html)
             e.submit(send_mail, from_address, to_address, message)
         e.shutdown()
     except Exception as e:
